c4751a091ad25ecf52d4f7fd155eb85a38c2436f
jcaltamia <jayceealtamia@gmail.com>
2022-01-30 18:41:49 +0800

    added input functionality

--- app/js/script.js
+++ app/js/script.js
@@ -1,18 +1,61 @@
-let btn = document.getElementById('btn');
-const submitButton = document.getElementById('btn');
-const allInput= document.getElementById('myForm'); 
+const myForm = document.getElementById('myForm');
 
-const userInputs = allInput.querySelectorAll('input'); 
+const fName = document.getElementById('fname');
+const lName = document.getElementById('lname');
+const password = document.getElementById('password');
 
-const submitButtonHandler = () => {
-    const fName = userInputs[0].value;
-  if (fName.trim === '') {
-    console.log('empty');
-    return;
-  } else {
-    console.log(fName); 
-    return;
+const errorTextFname = document.getElementById('form__text-warning-fname');
+const errorTextLname = document.getElementById('form__text-warning-lname');
+const errorTextPassword = document.getElementById(
+  'form__text-warning-password'
+);
+
+const errorIconFname = document.getElementById('form__error-icon-fname');
+const errorIconLname = document.getElementById('form__error-icon-lname');
+const errorIconPassword = document.getElementById('form__error-icon-password');
+
+myForm.addEventListener('submit', (e) => {
+  if (fName.value === '' || fName.value == null) {
+    errorIconFname.classList.add('visible');
+    e.preventDefault();
+    errorTextFname.innerText = 'First Name cannot be empty';
+    fName.classList.add('form__input--warning');
   }
-};
 
-submitButton.addEventListener('click', submitButtonHandler);
+  if (lName.value === '' || lName.value == null) {
+    errorIconLname.classList.add('visible');
+    e.preventDefault();
+    errorTextLname.innerText = 'Last Name cannot be empty';
+    lName.classList.add('form__input--warning');
+  }
+
+  if (password.value === '' || password.value == null) {
+    errorIconPassword.classList.add('visible');
+    e.preventDefault();
+    errorTextPassword.innerText = 'Password cannot be empty';
+    password.classList.add('form__input--warning');
+  }
+});
+
+if (errorTextFname.value !== '' || errorTextFname.value != null) {
+  fName.addEventListener('input', (e) => {
+    errorIconFname.classList.remove('visible');
+    errorTextFname.innerText = '';
+    fName.classList.remove('form__input--warning');
+  });
+}
+if (errorTextLname.value !== '' || errorTextLname.value != null) {
+  lName.addEventListener('input', (e) => {
+    errorIconLname.classList.remove('visible');
+    errorTextLname.innerText = '';
+    lName.classList.remove('form__input--warning');
+  });
+}
+
+if (errorTextPassword.value !== '' || errorTextPassword.value != null) {
+  password.addEventListener('input', (e) => {
+    errorIconPassword.classList.remove('visible');
+    errorTextPassword.innerText = '';
+    password.classList.remove('form__input--warning');
+  });
+}
