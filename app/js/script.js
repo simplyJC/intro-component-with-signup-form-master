0484d1eed806375a32c4d9e2dff65e392bafb306
jcaltamia <jayceealtamia@gmail.com>
2022-02-01 04:49:40 +0800

    added alert box

--- app/js/script.js
+++ app/js/script.js
@@ -1,5 +1,7 @@
 const form = document.querySelector('.form__container form');
 const inputs = document.querySelectorAll('.form__container input');
+let fname;
+
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
@@ -8,9 +10,14 @@ form.addEventListener('submit', (e) => {
       input.parentElement.classList.add('error');
     } else {
       input.parentElement.classList.remove('error');
+      if (input.name == 'fname') {
+        fname = input.value;
+        console.log('Hello', +fname);
+      }
       if (input.type == 'email') {
         if (validateEmail(input.value)) {
           input.parentElement.classList.remove('error');
+          alert('Hello ' + fname + ' ');
         } else {
           input.parentElement.classList.add('error');
         }
@@ -19,14 +26,17 @@ form.addEventListener('submit', (e) => {
   });
 });
 
+/* A function to check if user input is valid email type */
 function validateEmail(email) {
   let emailCheck =
     /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return emailCheck.test(String(email).toLowerCase());
 }
 
+//[1] Remove  error warning when user click input
+
 inputs.forEach((input) => {
   input.addEventListener('input', () => {
-    input.parentElement.classList.remove('error');
+    input.parentElement.classList.remove('error'); /*1*/
   });
 });
